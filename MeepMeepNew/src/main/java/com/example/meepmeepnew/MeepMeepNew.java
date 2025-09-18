5939c0f7b66f749b3356fb95f7db8cbba18cf521
= <malcolmnelson08@protonmail.com>
2025-09-17 23:13:49 -0500

    Finished ppg path

--- MeepMeepNew/src/main/java/com/example/meepmeepnew/MeepMeepNew.java
+++ MeepMeepNew/src/main/java/com/example/meepmeepnew/MeepMeepNew.java
@@ -14,7 +14,8 @@ public class MeepMeepNew {
         Pose2d startPos = new Pose2d(61, -20, toRadians(180));
 
         Vector2d launchVec = new Vector2d(55, -15);
-        double launchHeading = toRadians(205);
+        double launchHeading = toRadians(200);
+        Pose2d launchPose = new Pose2d(launchVec.x, launchVec.y, launchHeading);
 
         MeepMeep meepMeep = new MeepMeep(800);
 
@@ -23,7 +24,7 @@ public class MeepMeepNew {
             .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
             .build();
 
-        Action pgp = new SequentialAction(myBot.getDrive().actionBuilder(startPos)
+        Action pgp = myBot.getDrive().actionBuilder(startPos)
                 .waitSeconds(2)
             .strafeToLinearHeading(launchVec, launchHeading)
             .waitSeconds(3)
@@ -31,14 +32,15 @@ public class MeepMeepNew {
             .splineToLinearHeading(new Pose2d(35, -50, toRadians(-90)), toRadians(-90))
             .strafeToLinearHeading(launchVec, launchHeading)
             .waitSeconds(3)
+
             .splineToLinearHeading(new Pose2d(12, -50, toRadians(-90)), toRadians(-90))
             .strafeToLinearHeading(new Vector2d(0, -60), toRadians(180))
             .waitSeconds(3)
             .strafeToLinearHeading(launchVec, launchHeading)
             .waitSeconds(3)
-            .build());
+            .build();
 
-        Action gpp = new SequentialAction(myBot.getDrive().actionBuilder(startPos)
+        Action gpp = myBot.getDrive().actionBuilder(startPos)
                 .waitSeconds(2)
             .strafeToLinearHeading(launchVec, launchHeading)
             .waitSeconds(4)
@@ -47,27 +49,37 @@ public class MeepMeepNew {
             .lineToY(-50)
             .strafeToLinearHeading(launchVec, launchHeading)
             .waitSeconds(3)
+
             .splineToLinearHeading(new Pose2d(36, -40, toRadians(-90)), toRadians(-90))
             .lineToY(-50)
             .strafeToLinearHeading(new Vector2d(0, -60), toRadians(180))
             .waitSeconds(3)
             .strafeToLinearHeading(launchVec, launchHeading)
             .waitSeconds(3)
-            .build());
+            .build();
 
-        Action ppg = new SequentialAction(myBot.getDrive().actionBuilder(startPos)
-                .waitSeconds(2)
+        Action ppg = myBot.getDrive().actionBuilder(startPos)
+            .waitSeconds(2)
             .strafeToLinearHeading(launchVec, launchHeading)
-                .waitSeconds(4)
-            .setTangent(toRadians(180))
-            .splineToLinearHeading(new Pose2d(12, -40, toRadians(-90)), toRadians(-90))
-            .lineToY(-50)
-            .splineToLinearHeading(new Pose2d(55, -14, toRadians(-205)), toRadians(-90))
-                .waitSeconds(3)
-            .splineToLinearHeading(new Pose2d(0, -60, toRadians(180)), toRadians(-90))
-            .splineToLinearHeading(new Pose2d(35, -70, toRadians(90)), toRadians(-90))
-            .build());
+            .waitSeconds(4)
 
+            .setTangent(toRadians(180))
+            .splineToSplineHeading(new Pose2d(12, -50, toRadians(-90)), toRadians(-90))
+            .setTangent(toRadians(90))
+            .waitSeconds(0.5)
+            .splineToLinearHeading(launchPose, 0)
+            .waitSeconds(4)
+
+            .splineToLinearHeading(new Pose2d(7, -54, 0), toRadians(-90))
+            .waitSeconds(2)
+            .lineToX(23)
+            .setTangent(0)
+            .splineToSplineHeading(new Pose2d(33, -40, toRadians(100)), toRadians(90))
+            .setTangent(toRadians(90))
+            .splineToSplineHeading(launchPose, toRadians(45))
+            .waitSeconds(4)
+            .lineToX(launchVec.x+1)
+            .build();
 
             myBot.runAction(ppg);
 
